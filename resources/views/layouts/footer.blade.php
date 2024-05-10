b8d752dc748e7da621193d9c3a271279dd3309b0
Samin Yeasar <samin@Samins-MacBook-Air.local>
2024-05-10 14:34:58 +0600

    stock-portal-sample-code-v1 code-removed 10/05/24

--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> {{$setting->brand_name }}.</p>            </div>
-        </div>
-    </div>
-</footer>
